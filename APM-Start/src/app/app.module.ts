863764b7e398b090361d70884ac3d58395560c4b
Mustafa Saad <m.saad@gama-ksa.com>
2022-03-21 17:47:16 +0300

    Chapter 6

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 
@@ -10,7 +10,8 @@ import { ProductsListComponent } from './products-list/products-list.component';
     ProductsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
--- APM-Start/src/app/products-list/products-list.component.ts
+++ APM-Start/src/app/products-list/products-list.component.ts
@@ -6,7 +6,11 @@ import { Component } from "@angular/core";
 })
 export class ProductsListComponent {
     pageTitle: string = 'Product List';
-    filter: string = "a";
+    filter: string = "cart";
+    imageWidth = 50;
+    imageMargin = 2;
+    showImages = false;
+
     products: any[] = [
         {
             "productId": 1,
@@ -59,4 +63,8 @@ export class ProductsListComponent {
             "imageUrl": "assets/images/xbox-controller.png"
         }
     ];
+
+    toggleImage(): void {
+        this.showImages = !this.showImages;
+    }
 }
